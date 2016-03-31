3ed56bd93ffe9438510d6c989e29b8ac0a1eaef7
francois D'Agostini <francoisdagostini@MacBook-Pro-de-francois.local>
2016-03-31 22:29:48 +0200

    added more screen infos

--- app_client/epg/epg.controller.js
+++ app_client/epg/epg.controller.js
@@ -2,7 +2,7 @@ angular.module('infiniteSocialApp').controller('epgCtrl', function($scope) {
 
     var vm = this;
     vm.pageHeader = {
-        title: "Ecran de l'EPG"
+        title: "EPG Screen"
     };
 
 });
