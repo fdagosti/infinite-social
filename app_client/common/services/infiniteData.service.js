e052ab0119ab038271061d63d40302c69067f3b8
francois D'Agostini <francoisdagostini@MacBook-Pro-de-francois.local>
2016-04-01 12:17:42 +0200

    added profile video recos

--- app_client/common/services/infiniteData.service.js
+++ app_client/common/services/infiniteData.service.js
@@ -93,16 +93,112 @@
         currentProfile : 0,
         profileList: [
         {
-          name : "Francois"
+          name : "Francois",
+          videoRecos : [
+    [
+        {
+            img: "http://media.senscritique.com/media/000012353851/source_big/Mission_Impossible.jpg",
+            title: "Mission Impossible",
+            description: "bla bli blou"
         },
         {
-          name : "Rachid"
+            img: "http://media.senscritique.com/media/000008032023/source_big/Le_Bon_la_Brute_et_le_Truand.jpg",
+            title: "Le bon la brute et le truand",
+            description: "bla bli blou"
         },
         {
-          name : "Thomas"
+            img: "http://fr.web.img6.acsta.net/medias/nmedia/18/36/10/96/19649758.jpg",
+            title: "Le diner de cons",
+            description: "bla bli blou"
         },
         {
-          name : "Mohammed"
+            img: "http://frothygirlz.com/wp-content/uploads/2009/07/brazil.jpg",
+            title: "Brazil",
+            description: "bla bli blou"
+        },
+
+    ]],
+        },
+        {
+          name : "Rachid",
+          videoRecos: [
+    [
+        {
+            img: "https://cnet4.cbsistatic.com/hub/i/r/2015/11/19/a899c919-8f1d-446f-a74c-5615651677d9/resize/570xauto/2ff9db5646572d3ad0c18430576a09ed/star-wars-poster-1977.jpg",
+            title: "Star Wars",
+            description: "bla bli blou"
+        },
+        {
+            img: "http://www.loopio.fr/images/rubriques/cat-petit-pony.jpg",
+            title: "Mon petit poney",
+            description: "bla bli blou"
+        },
+        {
+            img: "http://img15.hostingpics.net/pics/224122canalmanga4820.jpg",
+            title: "One Piece",
+            description: "bla bli blou"
+        },
+        {
+            img: "http://www.topnormandie.com/wp-content/uploads/2015/11/La-Reine-des-neiges-est-le-plus-gros-succes-de-tous-les-temps-pour-un-film-d-animation_portrait_w532.jpg",
+            title: "la reine des neiges",
+            description: "bla bli blou"
+        },
+
+    ]],
+        },
+        {
+          name : "Thomas",
+          videoRecos: [
+    [
+        {
+            img: "http://fr.web.img2.acsta.net/pictures/13/12/18/10/01/110035.jpg",
+            title: "La belle et la Bete",
+            description: "bla bli blou"
+        },
+        {
+            img: "http://www.commeaucinema.com/images/news_insert/348498_799771_266519.jpg",
+            title: "Conan le barbare",
+            description: "bla bli blou"
+        },
+        {
+            img: "http://images.jedessine.com/_uploads/_tiny_galerie/20130209/vign-tchoupi-mku_fe7.jpg",
+            title: "Tchoupi",
+            description: "bla bli blou"
+        },
+        {
+            img: "http://images.fan-de-cinema.com/affiches/large/99/25243.jpg",
+            title: "Ce que veulent les femmes",
+            description: "bla bli blou"
+        },
+
+    ]],
+        },
+        {
+          name : "Mohammed",
+          videoRecos: [
+    [
+        {
+            img: "http://static.srcdn.com/slir/w700-h350-q90-c700:350/wp-content/uploads/Dragon-Ball-Super-Anime.jpg",
+            title: "Dragon Ball",
+            description: "bla bli blou"
+        },
+        {
+            img: "http://ia.media-imdb.com/images/M/MV5BMTk2NTI1MTU4N15BMl5BanBnXkFtZTcwODg0OTY0Nw@@._V1_SX640_SY720_.jpg",
+            title: "The Avengers",
+            description: "bla bli blou"
+        },
+        {
+            img: "http://fr.web.img6.acsta.net/medias/nmedia/18/88/95/51/20264212.jpg",
+            title: "James Bond",
+            description: "bla bli blou"
+        },
+        {
+            img: "http://fr.web.img6.acsta.net/medias/nmedia/00/02/54/95/affiche2.jpg",
+            title: "Lord of the rings",
+            description: "bla bli blou"
+        },
+
+    ]],
         },
        ]
      };
